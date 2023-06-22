82c890d794190cddbb0a54c4e74c66492f3abf4b
ultragrace <udechukwuakachukwu.com>
2023-06-22 23:44:34 +0100

    Created the header component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import HomePage from './pages/HomePage'
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1 className="text-red-500 text-5xl">Blog App</h1>
-    </>
+    <div>
+      <HomePage />
+    </div>
   );
 }
 
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainLayout from '../components/MainLayout'
+
+const HomePage = () => {
+  return (
+    <MainLayout>
+        
+    </MainLayout>
+  )
+}
+
+export default HomePage
